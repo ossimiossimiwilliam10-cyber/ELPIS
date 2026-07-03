0b76ffa22ecf7fbc7b6bdb3d729b2917e8d2b2c0
Williamos <ossimiossimiwilliam10@gmail.com>
2026-07-03 13:23:38 +0200

    fix(server): increase express json limit to 50mb to allow large cours.json

--- interface/bridge/server.js
+++ interface/bridge/server.js
@@ -43,7 +43,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Sécurité : Basic Auth (Point 2)
 if (process.env.ADMIN_PASSWORD) {
